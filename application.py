11801e6093dfdf5a755507b56c9b4de30695394c
Farhan <farhanmnavas@gmail.com>
2023-11-01 05:42:30 +0800

    Build main page and about me page

---
+++ application.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/')
+def index():
+    return 'Hello Backend'
+
+@app.route('/about_me')
+def about_us():
+    name = "Mohamed Navas Farhan"
+    course = "Computer Science"
+    year = "1"
+    ccas = ["RH Devs", "Ultimate", "HPB", "RVC Pioneers"]
+    ccas_str = ', '.join(ccas)
+
+    return f"Name: {name} <br> Course: {course} <br> Year: {year} <br> CCA's: {ccas_str}"
+
+if __name__ == '__main__':
+    app.run()
